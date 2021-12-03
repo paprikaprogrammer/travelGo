c6278b5345aa2743957f15bd02ed95b52a6c9887
tiput <tiara.putri@atozpay.co.id>
2021-12-03 10:20:38 +0700

    add assets & package

--- src/consts/colors.js
+++ src/consts/colors.js
@@ -1,12 +1,13 @@
 const COLORS = {
   white: '#FFF',
   dark: '#000',
-  primary: '#04555c',
+  primarytosca: '#04555c',
   secondary: '#e1e8e9',
   light: '#f9f9f9',
   grey: '#dddedd',
   red: 'red',
   orange: '#f5a623',
+  primary : '#7445ad'
 };
 
 export default COLORS;
