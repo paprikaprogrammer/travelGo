3677d52c96e250281eb6e5eec4b00a89e549580f
tiput <tiara.putri@atozpay.co.id>
2021-12-08 09:47:59 +0700

    add navigation details

--- src/Navigations/index.js
+++ src/Navigations/index.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import OnBoardScreen from '../views/screens/OnBoardScreen';
 import HomeScreen from '../views/screens/HomeScreen';
+import DetailsScreen from '../views/screens/DetailsScreen';
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Stack.Navigator  screenOptions={{headerShown: false}}>
         <Stack.Screen name="OnBoardScreen" component={OnBoardScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
