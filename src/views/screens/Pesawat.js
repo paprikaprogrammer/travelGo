7e326269b4e12c7acf87f97dc963dac8d5622c38
tiput <tiara.putri@atozpay.co.id>
2022-01-19 17:52:06 +0700

    fix layout screen flight & hotel

--- src/views/screens/Pesawat.js
+++ src/views/screens/Pesawat.js
@@ -1,106 +1,111 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  Dimensions,
-  Button,
-} from 'react-native';
-import Icons from 'react-native-vector-icons/Entypo';
+import React from 'react'
+import { 
+    Text, 
+    ScrollView,
+    ImageBackground,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    TextInput,
+    Button,
+    View, } from 'react-native'
 import COLORS from '../../consts/colors';
-const {width} = Dimensions.get('screen');
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icons from 'react-native-vector-icons/Entypo';
 
-const Pesawat = () => {
-  return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
-      <StatusBar translucent={false} backgroundColor={COLORS.primary} />
-      <View style={style.header}>
-      </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View
-          style={{
-            backgroundColor: COLORS.primary,
-            height: 500,
-            paddingHorizontal: 20,
-          }}>
-          <View style={style.box}>
-            <Text>Dari :</Text>
-            <View style={style.inputContainer}>
-              <Icons name="aircraft" size={28} />
-              <TextInput
-                placeholder="Jakarta"
-                style={{color: COLORS.grey}}
-              />
-            </View>
-            <Text>Ke :</Text>
-            <View style={style.inputContainer}>
-              <Icons name="aircraft" size={28} />
-              <TextInput
-                placeholder="Bandung"
-                style={{color: COLORS.grey}}
-              />
-            </View>
-            <Text>Tanggal Berangkat :</Text>
-            <View style={style.inputContainer}>
-              <Icons name="aircraft" size={28} />
-              <TextInput
-                placeholder="Tanggal"
-                style={{color: COLORS.grey}}
-              />
-            </View>
-            <View>
-              <Text>Penumpang :</Text>
-              <View style={style.inputContainer}>
-                <Icons name="aircraft" size={28} />
-                <TextInput
-                  placeholder="1 Penumpang"
-                  style={{color: COLORS.grey}}
-                />
-              </View>
-              <Text>Kelas Penerbangan :</Text>
-              <View style={style.inputContainer}>
-                <Icons name="aircraft" size={28} />
-                <TextInput
-                  placeholder="Kelas"
-                  style={{color: COLORS.grey}}
-                />
-              </View>
-            </View>
-            <Button title="Cari Tiket" style={style.button}></Button>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+const Pesawat = ({navigation}) => {
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+            <StatusBar translucent backgroundColor={COLORS.primary} />
+            <ImageBackground style={{flex: 1, height: 130}} source={require('../../assets/pesawat.png')}>
+                <View style={style.header}>
+                    <Icon
+                        name="arrow-back-ios"
+                        size={28}
+                        color={COLORS.primary}
+                        onPress={navigation.goBack}
+                    />
+                </View>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <View
+                    style={style.containers}>
+                    <View>
+                      <Text>Dari :</Text>
+                      <View style={style.inputContainer}>
+                        <Icons name="aircraft" size={28} />
+                        <TextInput
+                          placeholder="Jakarta"
+                          style={{color: COLORS.grey}}
+                        />
+                      </View>
+                      <Text>Ke :</Text>
+                      <View style={style.inputContainer}>
+                        <Icons name="aircraft" size={28} />
+                        <TextInput
+                          placeholder="Bandung"
+                          style={{color: COLORS.grey}}
+                        />
+                      </View>
+                      <Text>Tanggal Berangkat :</Text>
+                      <View style={style.inputContainer}>
+                        <Icons name="aircraft" size={28} />
+                        <TextInput
+                          placeholder="Tanggal"
+                          style={{color: COLORS.grey}}
+                        />
+                      </View>
+                      <View>
+                        <Text>Penumpang :</Text>
+                        <View style={style.inputContainer}>
+                          <Icons name="aircraft" size={28} />
+                          <TextInput
+                            placeholder="1 Penumpang"
+                            style={{color: COLORS.grey}}
+                          />
+                        </View>
+                        <Text>Kelas Penerbangan :</Text>
+                        <View style={style.inputContainer}>
+                          <Icons name="aircraft" size={28} />
+                          <TextInput
+                            placeholder="Kelas"
+                            style={{color: COLORS.grey}}
+                          />
+                        </View>
+                      </View>
+                      <Button title="Cari Tiket" style={style.button}></Button>
+                    </View>
+                    </View>
+                </ScrollView>
+            </ImageBackground>
+        </SafeAreaView>
+    )
+}
 
 const style = StyleSheet.create({
-  header: {
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    backgroundColor: COLORS.primary,
-  },
-  inputContainer: {
-    height: 55,
-    flexDirection: 'row',
-    paddingHorizontal: 10,
-    alignItems: 'center',
-  },
-  box: {
-    elevation: 15,
-    borderRadius: 10,
-    paddingHorizontal: 2,
-    backgroundColor: COLORS.white,
-    height: 450
-  },
-  button: {
-    paddingHorizontal: 10,
-  }
+    header: {
+        marginTop: 60,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+    },
+    containers: {
+      backgroundColor: COLORS.white,
+      height: 700,
+      paddingHorizontal: 20,
+      borderTopStartRadius: 30,
+      borderTopEndRadius: 30,
+      paddingTop: 10,
+      marginTop: 40,
+      elevation: 20
+    },
+    inputContainer: {
+      height: 55,
+      flexDirection: 'row',
+      paddingHorizontal: 10,
+      alignItems: 'center',
+      borderWidth: 0.5,
+      margin: 10
+    },
 });
-export default Pesawat;
+
+export default Pesawat
