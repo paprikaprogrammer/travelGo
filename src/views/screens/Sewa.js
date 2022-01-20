bcb4be2e5100aac1f4fdfe15405a2a38ce6f9512
tiput <tiara.putri@atozpay.co.id>
2022-01-20 19:48:23 +0700

    create screen sewa

---
+++ src/views/screens/Sewa.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { 
+    Text, 
+    ScrollView,
+    ImageBackground,
+    SafeAreaView,
+    StatusBar,
+    TextInput,
+    Button,
+    StyleSheet,
+    View, } from 'react-native'
+import COLORS from '../../consts/colors';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icons from 'react-native-vector-icons/Entypo';
+
+const Sewa = ({navigation}) => {
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+            <StatusBar translucent backgroundColor={COLORS.primary} />
+            <ImageBackground style={{flex: 1, height: 130}} source={require('../../assets/mobil.png')}>
+                <View style={style.header}>
+                    <Icon
+                        name="arrow-back-ios"
+                        size={28}
+                        color={COLORS.primary}
+                        onPress={navigation.goBack}
+                    />
+                </View>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <View
+                    style={style.containers}>
+                    <View>
+                    <Text>Lokasi Jemput :</Text>
+                      <View style={style.inputContainer}>
+                        <Icons name="" size={28} />
+                        <TextInput
+                          placeholder="Hotel"
+                          style={{color: COLORS.grey}}
+                        />
+                      </View>
+                      <Text>Durasi :</Text>
+                      <View style={style.inputContainer}>
+                        <Icons name="" size={28} />
+                        <TextInput
+                          placeholder="1, kamar"
+                          style={{color: COLORS.grey}}
+                        />
+                      </View>
+                      <Button title="Cari Tiket" style={style.button}></Button>
+                    </View>
+                    </View>
+                </ScrollView>
+            </ImageBackground>
+        </SafeAreaView>
+    )
+}
+
+const style = StyleSheet.create({
+  header: {
+      marginTop: 60,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingHorizontal: 20,
+  },
+  inputContainer: {
+    height: 55,
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    borderWidth: 0.5,
+    margin: 10
+  },
+  containers: {
+      backgroundColor: COLORS.white,
+      height: 500,
+      paddingHorizontal: 20,
+      borderTopStartRadius: 30,
+      borderTopEndRadius: 30,
+      paddingTop: 10,
+      marginTop: 40,
+      elevation: 20
+  },
+  button: {
+    paddingHorizontal: 10,
+    marginTop: 30,
+    paddingTop:20
+  }
+});
+
+export default Sewa
