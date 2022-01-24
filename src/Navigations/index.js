0fc9b661d6eb23fafa924f9332026dbfe74e8fdf
tiput <tiara.putri@atozpay.co.id>
2022-01-24 18:12:01 +0700

    add navigation

--- src/Navigations/index.js
+++ src/Navigations/index.js
@@ -1,8 +1,16 @@
 import * as React from 'react';
+import { Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import OnBoardScreen from '../views/screens/OnBoardScreen';
+
+// tabs
 import HomeScreen from '../views/screens/HomeScreen';
+import Order from '../page/home/order/orders';
+import Explore from '../page/home/explore/explores';
+import Akun from '../page/home/akun/akuns';
+// tabs
+
 import DetailsScreen from '../views/screens/DetailsScreen';
 import Wallet from '../views/screens/Wallet';
 import DirectWebview from '../views/screens/DirectWebview';
@@ -16,14 +24,17 @@ import Eat from '../views/screens/Eat'
 import Event from '../views/screens/Event'
 import Sewa from '../views/screens/Sewa'
 import Promo from '../views/screens/Promo'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator()
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator  screenOptions={{headerShown: false}}>
         <Stack.Screen name="OnBoardScreen" component={OnBoardScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={Tab1} />
         <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
         <Stack.Screen name="Wallet" component={Wallet} />
         <Stack.Screen name="DirectWebview" component={DirectWebview} />
@@ -43,3 +54,78 @@ function App() {
 }
 
 export default App;
+
+export function Tab1() {
+  return (
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen name="Home" component={HomeScreen} 
+      options={{
+        tabBarIcon:({focused}) => (
+          <View>
+            <Image 
+            source={require('../assets/ichome.png')}
+            resizeMode='contain'
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: focused ? '#439edf' : '#747474'
+            }}>
+            </Image>
+          </View>
+        ),
+      }}
+      />
+      <Tab.Screen name="Order" component={Order}
+      options={{
+        tabBarIcon:({focused}) => (
+          <View>
+            <Image 
+            source={require('../assets/icorder.png')}
+            resizeMode='contain'
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: focused ? '#439edf' : '#747474'
+            }}>
+            </Image>
+          </View>
+        ),
+      }}
+      />
+      <Tab.Screen name="Explore" component={Explore}
+      options={{
+        tabBarIcon:({focused}) => (
+          <View>
+            <Image 
+            source={require('../assets/icexplore.png')}
+            resizeMode='contain'
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: focused ? '#439edf' : '#747474'
+            }}>
+            </Image>
+          </View>
+        ),
+      }}
+      />
+      <Tab.Screen name="Akun" component={Akun}
+      options={{
+        tabBarIcon:({focused}) => (
+          <View>
+            <Image 
+            source={require('../assets/icuser.png')}
+            resizeMode='contain'
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: focused ? '#439edf' : '#747474'
+            }}>
+            </Image>
+          </View>
+        ),
+      }}
+      />
+    </Tab.Navigator>
+  )
+}
