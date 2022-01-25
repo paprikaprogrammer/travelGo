3dd819dc69671a223f251f287f3b5ce1967a3845
typut19 <typut19@gmail.com>
2022-01-25 22:28:23 +0700

    continue layouting screen explore & order

--- src/page/home/order/orders.js
+++ src/page/home/order/orders.js
@@ -1,121 +1,85 @@
-import React from 'react'
-import { 
-    Text, 
-    ScrollView,
-    ImageBackground,
-    SafeAreaView,
-    StatusBar,
-    Image,
-    StyleSheet,
-    Dimensions,
-    View, } from 'react-native'
-import COLORS from '../../consts/colors';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-const {width} = Dimensions.get('screen');
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+} from 'react-native';
+import COLORS from '../../../consts/colors';
 
-const explores = ({navigation}) => {
-    return (
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
-            <StatusBar translucent backgroundColor={COLORS.primary} />
-            <ImageBackground style={{flex: 1, height: 130}} source={require('../../assets/todo.png')}>
-                <View style={style.header}>
-                    <Icon
-                        name="arrow-back-ios"
-                        size={28}
-                        color={COLORS.primary}
-                        onPress={navigation.goBack}
-                    />
-                </View>
-                <ScrollView showsVerticalScrollIndicator={false}>
-                    <View
-                    style={style.containers}>
-                    <View style={style.wrapTitle}>
-                        <Text style={style.title}>Hore! Mari kita liburan..</Text>
-                        <Text style={style.titleFav}>Tempat farovit kembali dibuka dan aman dikunjungi. Yuk, Pesan sekarang</Text>
-                    </View>
-                    <View style={style.wrapper}>
-                        <Image style={style.warpImg} source={require('../../assets/todo1.jpg')}></Image>
-                        <View style={style.wrapBoxTxt}>
-                            <View>
-                                <Text style={style.warpTxt}>Voucher Kidania</Text>
-                                <Text style={{fontSize:11}}>Bandung</Text>
-                            </View>
-                        </View>
-                    </View>
-                    </View>
-                </ScrollView>
-            </ImageBackground>
-        </SafeAreaView>
-    )
-}
+const orders = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <StatusBar translucent={false} backgroundColor={COLORS.primary} />
+      <View style={style.header}>
+        <Text style={style.headerTxt}>My Order</Text>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={style.sectionContent}>
+            <Image style={style.sectionImg} source={require('../../../assets/bg_order.png')}></Image>
+            <View style={style.wrapTitle}>
+                <Text style={style.title}>Hore! Mari kita liburan..</Text>
+                <Text style={style.titleFav}>Setelah memesan perjalanan, kamu bisa mengatur pesanan dan melihat E-tiketmu di sini.</Text>
+            </View> 
+            <View style={style.btnTop}>
+                <Button color={COLORS.primary} title='Pesan Perjalanan'></Button>
+            </View>
+            <View style={style.btnTop}>
+                <Button color={COLORS.primary} title='Lihat Riwayat Pesanan'></Button>
+            </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const style = StyleSheet.create({
     header: {
-        marginTop: 60,
+        paddingVertical: 20,
+        paddingHorizontal: 20,
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
+        justifyContent: 'flex-start',
+        backgroundColor: COLORS.primary,
     },
-    containers: {
-        backgroundColor: COLORS.white,
-        height: 500,
-        paddingHorizontal: 20,
-        borderTopStartRadius: 30,
-        borderTopEndRadius: 30,
-        paddingTop: 10,
-        marginTop: 40,
-        elevation: 20
+    headerTxt:{
+        color: 'white',
+        fontSize: 17,
+        fontWeight: '500',
     },
-    wrapper: {
-        backgroundColor: 'white',
-        elevation: 4,
-        borderRadius: 8,
-        width: width / 2 - 27,
-        marginRight: 18,
-        marginBottom: 18,
-        marginTop: 5
-    },
-    warpImg :{
-        height: width/2-27,
-        width: width/2-27,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10
-    },
-    warpTxt:{
-        // marginLeft: 8,
-        // marginTop: 10,
+    sectionContent: {
+        marginHorizontal: 20,
+        marginVertical: 38,
         fontWeight: 'bold',
+        fontSize: 20,
     },
-    wrapBoxTxt :{
-        marginLeft: 8,
-        marginTop: 5,
-        marginBottom: 5
-    },
-    wrapTxtIdr: {
-        color: 'red',
-        fontWeight: 'bold'
-    },
-    wrapTxtNormalPrice : {
-        textDecorationLine: 'line-through', 
-        textDecorationStyle: 'solid',
-        fontSize: 11
-    },  
-    wrapDisc: {
-        position: 'absolute',
-        top: 10,
-        backgroundColor: 'white',
-        padding: 4,
+    sectionImg: {
+        width: 310,
+        height: 100,
+        marginTop: 35
     },
     wrapTitle :{
-        marginVertical: 13
+        marginVertical: 13,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     title :{
         fontWeight: 'bold',
-        fontSize: 18
+        fontSize: 19,
+        marginTop: 30
     },
     titleFav :{
-        fontSize: 13
+        fontSize: 13,
+        marginTop: 5,
+        textAlign: 'center',
+        paddingHorizontal: 15
+    },
+    btnTop:{
+        padding: 10,
+        margin: 3
     }
 });
-
-export default explores
+export default orders;
