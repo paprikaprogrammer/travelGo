c018964db8dd6c9ec86f9f5a20ff5592b257d446
typut19 <typut19@gmail.com>
2022-01-26 23:02:48 +0700

    implementasi api orders & fix layout

--- src/page/home/order/orders.js
+++ src/page/home/order/orders.js
@@ -12,29 +12,44 @@ import {
 import COLORS from '../../../consts/colors';
 
 const orders = () => {
-  return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
-      <StatusBar translucent={false} backgroundColor={COLORS.primary} />
-      <View style={style.header}>
-        <Text style={style.headerTxt}>My Order</Text>
-      </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={style.sectionContent}>
-            <Image style={style.sectionImg} source={require('../../../assets/bg_order.png')}></Image>
-            <View style={style.wrapTitle}>
-                <Text style={style.title}>Hore! Mari kita liburan..</Text>
-                <Text style={style.titleFav}>Setelah memesan perjalanan, kamu bisa mengatur pesanan dan melihat E-tiketmu di sini.</Text>
-            </View> 
-            <View style={style.btnTop}>
-                <Button color={COLORS.primary} title='Pesan Perjalanan'></Button>
+  render() {
+    componentDidMount() {
+        this.getRecomen()
+    }
+
+    getRecomen = async () => {
+        try {
+        const response = await Axios.get(`https://mocki.io/v1/475d2690-d114-450f-9e5a-0a2f803e1f44`)
+        this.setState({ isError: false, isLoading: false, data: response.data })
+        } catch (error) {
+        this.setState({ isLoading: false, isError: true })
+        }
+    }
+
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+          <StatusBar translucent={false} backgroundColor={COLORS.primary} />
+          <View style={style.header}>
+            <Text style={style.headerTxt}>My Order</Text>
+          </View>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={style.sectionContent}>
+                <Image style={style.sectionImg} source={{uri: `${item.image}`}}></Image>
+                <View style={style.wrapTitle}>
+                    <Text style={style.title}>Hore! Mari kita liburan..</Text>
+                    <Text style={style.titleFav}>Setelah memesan perjalanan, kamu bisa mengatur pesanan dan melihat E-tiketmu di sini.</Text>
+                </View> 
+                <View style={style.btnTop}>
+                    <Button color={COLORS.primary} title='Pesan Perjalanan'></Button>
+                </View>
+                <View style={style.btnTop}>
+                    <Button color={COLORS.primary} title='Lihat Riwayat Pesanan'></Button>
+                </View>
             </View>
-            <View style={style.btnTop}>
-                <Button color={COLORS.primary} title='Lihat Riwayat Pesanan'></Button>
-            </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+          </ScrollView>
+        </SafeAreaView>
+      );
+  }
 };
 
 const style = StyleSheet.create({
@@ -80,6 +95,15 @@ const style = StyleSheet.create({
     btnTop:{
         padding: 10,
         margin: 3
+    },
+    imageHead:{
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        backgroundColor: COLORS.primary,
+        marginTop: 5,
+        textAlign: 'center',
+        paddingHorizontal: 15
     }
 });
 export default orders;
